3639a4e4c823a2c3450fc89b73d989a2d491568a
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-10-08 23:04:29 -0300

    Endereco - Privando atributos, definindo metodos getters and setters

--- ECommerce/src/Endereco.java
+++ ECommerce/src/Endereco.java
@@ -1,10 +1,47 @@
 
 public class Endereco {
-	Integer id_endereco;
-	String rua;
-	String bairro;
-	String cidade;
-	String cep;
-	String pais;
+	private Integer id_endereco;
+	private String rua;
+	private String bairro;
+	private String cidade;
+	private String cep;
+	private String pais;
+	
+	public Integer getId_endereco() {
+		return id_endereco;
+	}
+	public void setId_endereco(Integer id_endereco) {
+		this.id_endereco = id_endereco;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
 	
 }
