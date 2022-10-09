f692081a8426a6e6a831b80eb8345d16e98d8ddb
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-10-08 23:16:09 -0300

    Produto -> Subindo classe Produto

---
+++ ECommerce/src/Produto.java
@@ -0,0 +1,46 @@
+
+public class Produto {
+	String nome;
+	String marca;
+	String categoria;
+	String descricao;
+	String imagem;
+	Integer id_produto;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getImagem() {
+		return imagem;
+	}
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+	public Integer getId_produto() {
+		return id_produto;
+	}
+	public void setId_produto(Integer id_produto) {
+		this.id_produto = id_produto;
+	}
+	
+}
