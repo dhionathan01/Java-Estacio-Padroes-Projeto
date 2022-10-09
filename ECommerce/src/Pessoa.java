4db5ea15bb432b25520bd0201dbdb06e64123979
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-10-08 22:53:05 -0300

    Subindo Classes para o github

---
+++ ECommerce/src/Pessoa.java
@@ -0,0 +1,47 @@
+
+public class Pessoa {
+	private String nome;
+	private String cpf;
+	private String telefone;
+	private String sexo;
+	private String dataNascimento;
+	private Integer id_pessoa;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public Integer getId_pessoa() {
+		return id_pessoa;
+	}
+	public void setId_pessoa(Integer id_pessoa) {
+		this.id_pessoa = id_pessoa;
+	}
+	
+}
---
+++ ECommerce/src/Usuario.java
@@ -0,0 +1,24 @@
+
+public class Usuario {
+	String email;
+	String senha;
+	Integer id_usuario;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public Integer getId_usuario() {
+		return id_usuario;
+	}
+	public void setId_usuario(Integer id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+}
