a97d1f0501b89de5ca67950dede37bb0490ad311
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-10-16 13:40:38 -0300

    Construtor Usuario

--- ECommerce/src/Usuario.java
+++ ECommerce/src/Usuario.java
@@ -1,8 +1,35 @@
 
-public class Usuario {
+public class Usuario  extends Pessoa{
+	
 	private Integer id_usuario;
 	private String email;
 	private String senha;
+	
+	 Usuario(Integer id_usuario,
+			 String email,
+			 String senha,
+			 Integer id_pessoa,
+			 String nome,
+			 String cpf,
+			 String telefone,
+			 String sexo,
+			 String dataNascimento){
+		super(id_pessoa, nome,cpf,telefone,sexo,dataNascimento);
+		this.id_usuario = id_usuario;
+		this.email = email;
+		this.senha = senha;
+	}
+	
+	public void exibirInformacoes() {
+		System.out.println("Email: " + this.email);
+		System.out.println("senha: " + this.senha);
+		System.out.println("Nome: " + this.getNome());
+		System.out.println("cpf: " + this.getCpf());
+		System.out.println("Telefone: " + this.getTelefone());
+		System.out.println("Sexo: " + this.getSexo());
+		System.out.println("Data Nascimento: " + this.getDataNascimento());
+		System.out.println("\n");
+	}
 	
 	public Integer getId_usuario() {
 		return id_usuario;
